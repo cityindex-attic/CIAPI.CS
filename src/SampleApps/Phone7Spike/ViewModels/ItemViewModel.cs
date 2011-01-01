af81f4d4ae39372d38004cf97fa5a1d2bb881845
bitpusher <sky.sanders@gmail.com>
2011-01-01 14:22:55 -0700

    Added simple phone 7 spike

---
+++ src/SampleApps/Phone7Spike/ViewModels/ItemViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Phone7Spike
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        private int _storyId;
+        public int StoryId
+        {
+            get
+            {
+                return _storyId;
+            }
+            set
+            {
+                if (_storyId == value)
+                    return;
+                _storyId = value;
+                NotifyPropertyChanged("StoryId");
+            }
+        }
+        private string _lineOne;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string LineOne
+        {
+            get
+            {
+                return _lineOne;
+            }
+            set
+            {
+                if (value != _lineOne)
+                {
+                    _lineOne = value;
+                    NotifyPropertyChanged("LineOne");
+                }
+            }
+        }
+
+        private string _lineTwo;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string LineTwo
+        {
+            get
+            {
+                return _lineTwo;
+            }
+            set
+            {
+                if (value != _lineTwo)
+                {
+                    _lineTwo = value;
+                    NotifyPropertyChanged("LineTwo");
+                }
+            }
+        }
+
+        private string _story;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Story
+        {
+            get
+            {
+                return _story;
+            }
+            set
+            {
+                if (value != _story)
+                {
+                    _story = value;
+                    NotifyPropertyChanged("Story");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ src/SampleApps/Phone7Spike/ViewModels/MainViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using CIAPI.Core;
+
+
+namespace Phone7Spike
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+
+
+
+            App.ctx.BeginListNewsHeadlines(result =>
+                {
+                    var response = App.ctx.EndListNewsHeadlines(result);
+                    Deployment.Current.Dispatcher.BeginInvoke(() =>
+                    {
+                        foreach (var item in response.Headlines)
+                        {
+                            Items.Add(new ItemViewModel() { LineOne = item.Headline, LineTwo = item.PublishDate, StoryId = item.StoryId });
+                        }
+                    });
+                    
+                    IsDataLoaded = true;
+                }, null, "UK", 20);
+
+                        
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
