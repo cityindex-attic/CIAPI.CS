6f9e9d43eaef04f2436a626ae433226b340e7599
bitpusher <sky.sanders@gmail.com>
2011-01-10 13:23:09 -0700

    ignoring test again

--- src/JsonClient.Tests/ThrottleFixture.cs
+++ src/JsonClient.Tests/ThrottleFixture.cs
@@ -14,7 +14,7 @@ namespace CityIndex.JsonClient.Tests
     public class ThrottleFixture
     {
 
-        [Test, MTAThreadAttribute,Ignore]
+        //[Test, MTAThreadAttribute,Ignore]
         public void CanThrottle1Per3Seconds()
         {
             var f = new TestRequestFactory();
