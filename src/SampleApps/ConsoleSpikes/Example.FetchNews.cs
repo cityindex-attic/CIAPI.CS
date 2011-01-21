51e2883757e8563f4d22d4577858130504429bdb
mrdavidlaing <david@davidlaing.com>
2011-01-21 12:11:04 +0000

    Add example on how to use streaming client

--- src/SampleApps/ConsoleSpikes/Example.FetchNews.cs
+++ src/SampleApps/ConsoleSpikes/Example.FetchNews.cs
@@ -2,15 +2,21 @@
 using System.Threading;
 using CIAPI;
 using CIAPI.DTO;
+using CIAPI.Streaming.Lightstreamer;
 
 namespace ConsoleSpikes
 {
     class Example
     {
+        private static readonly Uri RPC_URI = new Uri("https://ciapipreprod.cityindextest9.co.uk/tradingapi");
+        private static readonly Uri STREAMING_URI = new Uri("https://pushpreprod.cityindextest9.co.uk/CITYINDEXSTREAMING");
+        private const string USERNAME = "0x234";
+        private const string PASSWORD = "password";
+
         public void FetchNews_sync()
         {
-            var ctx = new CIAPI.Rpc.Client(new Uri("https://ciapipreprod.cityindextest9.co.uk/tradingapi"));
-            ctx.LogIn("0x234", "password");
+            var ctx = new CIAPI.Rpc.Client(RPC_URI);
+            ctx.LogIn(USERNAME, PASSWORD);
             
             ListNewsHeadlinesResponseDTO news = ctx.ListNewsHeadlines("UK", 10);
             
@@ -21,9 +27,9 @@ namespace ConsoleSpikes
 
         public void FetchNews_async()
         {
-            var ctx = new CIAPI.Rpc.Client(new Uri("https://ciapipreprod.cityindextest9.co.uk/tradingapi"));
+            var ctx = new CIAPI.Rpc.Client(RPC_URI);
             var gate = new AutoResetEvent(false);
-            ctx.BeginLogIn("0x234", "password", a =>
+            ctx.BeginLogIn(USERNAME, PASSWORD, a =>
             {
                 ctx.EndLogIn(a);
 
@@ -43,6 +49,43 @@ namespace ConsoleSpikes
             }, null);
 
             gate.WaitOne();
+        }
+
+        public void SubscribeToNewsHeadlineStream()
+        {
+            //First we need a valid session, obtained from the Rpc client
+            var ctx = new CIAPI.Rpc.Client(RPC_URI);
+            ctx.LogIn(USERNAME, PASSWORD);
+
+            //Next we create a connection to the streaming api, using the authenticated session
+            //You application should only ever have one of these
+            var streamingClient = new LightstreamerClient(STREAMING_URI, USERNAME, ctx.SessionId);
+            streamingClient.Connect();
+
+            //And instantiate a listener for news headlines on the appropriate topic
+            //You can have multiple listeners on one connection
+            var newsListener = streamingClient.BuildListener<NewsDTO>("NEWS.MOCKHEADLINES.UK");
+            newsListener.Start();
+
+            //The MessageRecieved event will be triggered every time a new News headline is available,
+            //so attach a handler for that event, and wait until something comes through
+            var gate = new ManualResetEvent(false);
+            NewsDTO recievedNewsHeadline = null;
+            newsListener.MessageRecieved += (s, e) =>
+            {
+                recievedNewsHeadline = e.Data;
+                //Do something with the new News headline data - perhaps update a news ticker?
+                gate.Set();
+            };
+            gate.WaitOne();
+
+            //Clean up
+            //Stop any listeners
+            newsListener.Stop();
+            //Shut down the connection
+            streamingClient.Disconnect();
+            //Destroy your session
+            ctx.LogOut();
         }
     }
 }
