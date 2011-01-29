99887ad7a3b0283c9819dc56f0a4fbefb81927e0
mrdavidlaing <david@davidlaing.com>
2011-01-29 17:02:52 +0000

    Import HttpUtility.UrlEncode from MIT licensed Mono Base Class library

---
+++ src/JsonClient.Tests/HttpUtilityFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace CityIndex.JsonClient.Tests
+{
+    [TestFixture]
+    public class HttpUtilityFixture
+    {
+        [Test]
+        public void UrlEncodeBehavesLikeBaseClassImplementation()
+        {
+            const string aString = "http://server.com/a/path/with a space/?param=1&arg1=true";
+            Assert.AreEqual(System.Web.HttpUtility.UrlEncode(aString), CityIndex.JsonClient.HttpUtility.UrlEncode(aString));
+        }
+    }
+}
