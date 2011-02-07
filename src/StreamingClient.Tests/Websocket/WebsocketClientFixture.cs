06fb82ba16ac73619622600b4b3c6b36b12d4310
mrdavidlaing <david@davidlaing.com>
2011-02-07 15:45:34 +0000

    Include very basic WebSocket client

---
+++ src/StreamingClient.Tests/Websocket/WebsocketClientFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CIAPI.Streaming;
+using NUnit.Framework;
+using StreamingClient.Websocket;
+
+namespace StreamingClient.Tests.Websocket
+{
+    [TestFixture]
+    public class WebSocketFixture
+    {
+       [Test, Category("DependsOnExternalResource")]
+       public void CanConnect()
+       {
+           var websocket = new WebsocketClient(new Uri("ws://ec2-50-16-152-101.compute-1.amazonaws.com:61614"));    
+           websocket.Connect();
+           var value = websocket.Recv();
+           websocket.Close();
+           Assert.IsNotNull(value);
+       }
+    }
+}
