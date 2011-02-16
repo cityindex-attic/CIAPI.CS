e13a21b8cc3fc5ff93c9d5832180bc75e86fe605
mrdavidlaing <david@davidlaing.com>
2011-02-16 12:56:39 +0000

    Test case for issue #4

--- src/CIAPI.IntegrationTests/Rpc/ErrorHandlingFixture.cs
+++ src/CIAPI.IntegrationTests/Rpc/ErrorHandlingFixture.cs
@@ -12,6 +12,10 @@ namespace CIAPI.IntegrationTests.Rpc
     [TestFixture]
     public class ErrorHandlingFixture
     {
+        private static Uri RPC_URI = new Uri("http://ciapipreprod.cityindextest9.co.uk/TradingApi");
+        private const string USERNAME_VALID = "DM904310";
+        private const string PASSWORD_VALID = "password";
+
         [Test]
         public void ShouldGiveGuidanceWhenSpecifyingInvalidServerName()
         {
@@ -22,7 +26,30 @@ namespace CIAPI.IntegrationTests.Rpc
             }
             catch (Exception ex)
             {
-                Assert.That(ex.Message, Is.StringContaining("DNS"),"Expecting some info explaining that the server name used cannot be found in DNS");                
+                Assert.That(ex, Is.TypeOf(typeof(ServerConnectionException)));
+                Assert.That(ex.Message, Is.StringContaining("server Url"),"Expecting some info explaining that the server Url is invalid");                
+            }
+        }
+
+        [Test]
+        public void LogOutShouldInvalidateSession()
+        {
+            var rpcClient = new Client(RPC_URI);
+            rpcClient.LogIn(USERNAME_VALID, PASSWORD_VALID);
+
+            var headlines = rpcClient.ListNewsHeadlines("UK", 3);
+            Assert.That(headlines.Headlines.Length, Is.GreaterThan(0), "you should have a set of headlines");
+
+            rpcClient.LogOut();
+
+            try
+            {
+                rpcClient.ListNewsHeadlines("AUS", 4);
+                Assert.Fail("the previous line should have thrown an (401) Unauthorized exception");
+            }
+            catch (ApiException e)
+            {
+                Assert.That(e.Message, Is.StringContaining("(401) Unauthorized"), "The session token should be invalid after logging out");
             }
         }
     }
