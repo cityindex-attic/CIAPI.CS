7ae9d7552f10ff44299aa71ac8756b58848932bc
bitpusher <sky.sanders@gmail.com>
2011-12-18 14:14:25 -0700

    create net3.5 projects

---
+++ src/JsonClient-3.5/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SubscriptionEngine.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SubscriptionEngine.Core")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c91f3e05-3514-4c92-b28a-80b94ef2cae6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("CityIndex.JsonClient.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b7bed198deeb5003cd33f655cd54ee5e4d55d54479e60929ad9a9ec563692cc1dd16859b0a1242f6b429f6f7f617467cf68ead7db2d10a857a5cb86cb913bfb85e2ee79ee20c2069c8d1119078fcd39ea5e5058e4764e13ba545e4447b50f03a3a073c52e503cf6811cd72b15d639f5bf6c4054aaf1aad93bdf83783929f9eb7")]
---
+++ src/StreamingClient/Lightstreamer/LightstreamerDefaults.cs
@@ -0,0 +1,7 @@
+﻿namespace StreamingClient.Lightstreamer
+{
+    public static class LightstreamerDefaults
+    {
+        public static int DEFAULT_TIMEOUT_MS = 30000;
+    }
+}
