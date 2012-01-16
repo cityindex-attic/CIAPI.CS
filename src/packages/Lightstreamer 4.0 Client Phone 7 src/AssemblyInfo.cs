5918f6a9f454e24d314eaa35a010586c51e42df7
David Laing <david@davidlaing.com>
2012-01-16 13:12:48 +0000

    Increment version to v2.0.0-bleeding as ListMarket related methods have new parameter useMobileShortcode

--- src/packages/Lightstreamer 4.0 Client Phone 7 src/AssemblyInfo.cs
+++ src/packages/Lightstreamer 4.0 Client Phone 7 src/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// Assembly WindowsPhoneClient, Version 1.0.4168.21541
 
-[assembly: System.Reflection.AssemblyVersion("1.14.0.0")]
+[assembly: System.Reflection.AssemblyVersion("2.0.0.0")]
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
