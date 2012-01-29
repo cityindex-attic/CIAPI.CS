0d68c792037735ef799a57f2563e085f1ca68b7f
mrdavidlaing <david@davidlaing.com>
2012-01-29 13:10:23 +0000

    Add extra fields to AMEEClient -> ItemValue DTO

--- src/AMEE/AMEEClient.CS/Model/ItemValue.cs
+++ src/AMEE/AMEEClient.CS/Model/ItemValue.cs
@@ -4,6 +4,18 @@ namespace AMEEClient.Model
 {
     public class ItemValue
     {
+        [JsonProperty("path")]
+        public string Path;
+        [JsonProperty("name")]
+        public string Name;
+        [JsonProperty("value")]
+        public string Value;
+        [JsonProperty("unit")]
+        public string Unit;
+        [JsonProperty("perUnit")]
+        public string PerUnit;
+        [JsonProperty("startDate")]
+        public string StartDate;
         [JsonProperty("itemValueDefinition")]
         public ItemValueDefinition ItemValueDefinition;
     }
