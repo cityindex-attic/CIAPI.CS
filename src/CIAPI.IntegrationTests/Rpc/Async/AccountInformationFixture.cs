77a8338481a7ef69b3e4fadc170525e925071cb1
bitpusher <sky.sanders@gmail.com>
2012-03-11 12:56:17 -0700

    adding symbol support

---
+++ src/CIAPI.IntegrationTests/Rpc/Async/AccountInformationFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using CIAPI.DTO;
+using CIAPI.IntegrationTests.Streaming;
+using CIAPI.Streaming;
+using Salient.JsonClient;
+using NUnit.Framework;
+using Client = CIAPI.Rpc.Client;
+
+namespace CIAPI.IntegrationTests.Rpc.Async
+{
+    [TestFixture]
+    public class AccountInformationFixture : RpcFixtureBase
+    {
+
+        [Test]
+        public void CanListOpenPositions()
+        {
+            var rpcClient = BuildRpcClient();
+
+            AccountInformationResponseDTO accounts = rpcClient.AccountInformation.GetClientAndTradingAccount();
+
+
+            rpcClient.TradesAndOrders.ListOpenPositions(accounts.TradingAccounts[0].TradingAccountId);
+            rpcClient.LogOut();
+            rpcClient.Dispose();
+
+        }
+
+ 
+    }
+}
