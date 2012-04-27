65723b751ce9636b7e087b00f72844be0cdc6b4b
David Laing <david@davidlaing.com>
2012-04-27 20:32:19 +0100

    #151 - Revert date format to MS style - /Date(37868276876)/

--- src/ReliableHttpClient/Salient.ReliableHttpClient.Serializer.Newtonsoft/Serializer.cs
+++ src/ReliableHttpClient/Salient.ReliableHttpClient.Serializer.Newtonsoft/Serializer.cs
@@ -15,9 +15,9 @@ namespace Salient.ReliableHttpClient.Serialization.Newtonsoft
 
         public string SerializeObject(object value)
         {
-            return JsonConvert.SerializeObject(value);
-
-
+            //Json.NET 4.5 changed the default dateformatting to ISO 8601 - http://james.newtonking.com/archive/2012/03/20/json-net-4-5-release-1-iso-dates-async-metro-build.aspx
+            //Change it back to the previous default of MS style dates i.e - /Date(19372927629377)/
+            return JsonConvert.SerializeObject(value, new JsonSerializerSettings{ DateFormatHandling = DateFormatHandling.MicrosoftDateFormat });
         }
 
         public T DeserializeObject<T>(string json)
