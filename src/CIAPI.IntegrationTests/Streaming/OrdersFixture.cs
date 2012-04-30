c7ac648401a66aa09263e931f297585d12cbc789
David Laing <david@davidlaing.com>
2012-04-30 16:51:14 +0100

    #131 - add sample app & integration test that replicates the error

---
+++ src/CIAPI.IntegrationTests/Streaming/OrdersFixture.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using CIAPI.DTO;
+using CIAPI.Streaming;
+using Salient.ReflectiveLoggingAdapter;
+using NUnit.Framework;
+
+
+namespace CIAPI.IntegrationTests.Streaming
+{
+    [TestFixture]
+    public class OrdersFixture : RpcFixtureBase
+    {
+        private ILog _logger = LogManager.GetLogger(typeof(PriceFixture));
+
+        /// <summary>
+        /// Test to replicate #131 - https://github.com/cityindex/CIAPI.CS/issues/131
+        /// </summary>
+        [Test]
+        public void CanRecieveOrderNotification()
+        {
+            var MARKET_ID = 154297; //GBP/USD
+            var gate = new ManualResetEvent(false);
+            var rpcClient = BuildRpcClient();
+            var streamingClient = rpcClient.CreateStreamingClient();
+            var priceListener = streamingClient.BuildPricesListener(MARKET_ID);
+            var ordersListener = streamingClient.BuildOrdersListener();
+
+            try
+            {
+                OrderDTO newOrder = null;
+
+                var marketInfo = rpcClient.Market.GetMarketInformation(MARKET_ID.ToString());
+                var account = rpcClient.AccountInformation.GetClientAndTradingAccount();
+
+                ordersListener.MessageReceived += (s, e) =>
+                    {
+                        newOrder = e.Data;
+                        Console.WriteLine(
+                            string.Format(
+                                "New order has been recieved on Orders stream\r\n {0}",
+                                e.Data.ToStringWithValues()));
+                        gate.Set();
+                    };
+
+                priceListener.MessageReceived += (s, e) =>
+                    {
+                        var order = new NewTradeOrderRequestDTO
+                                        {
+                                            MarketId = e.Data.MarketId,
+                                            BidPrice = e.Data.Bid,
+                                            OfferPrice = e.Data.Offer,
+                                            AuditId = e.Data.AuditId,
+                                            Quantity = marketInfo.MarketInformation.WebMinSize.GetValueOrDefault() + 1,
+                                            TradingAccountId = account.TradingAccounts[0].TradingAccountId,
+                                            Direction = "buy"
+                                        };
+
+                        var response = rpcClient.TradesAndOrders.Trade(order);
+                        rpcClient.MagicNumberResolver.ResolveMagicNumbers(response);
+                        Assert.AreEqual("Accepted", response.Status_Resolved);
+                    };
+
+
+                gate.WaitOne(TimeSpan.FromSeconds(15));
+
+                Assert.IsNotNull(newOrder);
+            }
+            finally
+            {
+                streamingClient.TearDownListener(priceListener);
+                streamingClient.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Samples/josemuniz-CIAPI.CS-orders-stream-problem-fcf46ed/MainPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using CIAPI.DTO;
+using CIAPI.Streaming;
+using Salient.ReflectiveLoggingAdapter;
+using StreamingClient;
+using Client = CIAPI.Rpc.Client;
+using IStreamingClient = CIAPI.Streaming.IStreamingClient;
+
+namespace PhoneApp3
+{
+    public partial class MainPage
+    {
+        private const string USERNAME = "DM813766";
+        private const string PASSWORD = "password";
+        private const int MarketId = 154297;
+
+        public Client RpcClient;
+        public IStreamingClient StreamingClient;
+        public IStreamingListener<PriceDTO> MarketPricesStream;
+        public IStreamingListener<OrderDTO> OrdersStream;
+        public ApiMarketInformationDTO Market;
+        public AccountInformationResponseDTO Account;
+        private bool _ordered;
+        private bool _listening;
+
+        private static readonly Uri RPC_URI = new Uri("https://ciapipreprod.cityindextest9.co.uk/TradingApi");
+        private static readonly Uri STREAM_URI = new Uri("https://pushpreprod.cityindextest9.co.uk/");
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Unloaded += OnMainPageUnloaded;
+            BuildClients();
+        }
+
+        private void OnMainPageUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (StreamingClient == null) return;
+            if (MarketPricesStream != null) StreamingClient.TearDownListener(MarketPricesStream);
+            if (OrdersStream != null) StreamingClient.TearDownListener(OrdersStream);
+        }        
+
+        private void BuildClients()
+        {
+            //Hook up a logger for the CIAPI.CS libraries
+            LogManager.CreateInnerLogger = (logName, logLevel, showLevel, showDateTime, showLogName, dateTimeFormat)
+                         => new SimpleDebugAppender(logName, logLevel, showLevel, showDateTime, showLogName, dateTimeFormat);
+
+            Dispatcher.BeginInvoke(() => listBox1.Items.Add("creating rpc client"));
+            RpcClient = new Client(RPC_URI, STREAM_URI, "CI-WP7");
+            RpcClient.BeginLogIn(USERNAME, PASSWORD, ar =>
+            {
+                var session = RpcClient.EndLogIn(ar);
+
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    Dispatcher.BeginInvoke(() => listBox1.Items.Add("creating listeners"));
+                    StreamingClient = RpcClient.CreateStreamingClient();
+                    MarketPricesStream = StreamingClient.BuildPricesListener(new[] { MarketId });
+                    MarketPricesStream.MessageReceived += OnMarketPricesStreamMessageReceived;
+                    OrdersStream = StreamingClient.BuildOrdersListener();
+                    OrdersStream.MessageReceived += OnOrdersStreamMessageReceived;
+
+                    Dispatcher.BeginInvoke(() => listBox1.Items.Add("getting account info"));
+                    RpcClient.AccountInformation.BeginGetClientAndTradingAccount(ar2 =>
+                    {
+                        Account = RpcClient.AccountInformation.EndGetClientAndTradingAccount(ar2);
+
+                        Dispatcher.BeginInvoke(() => listBox1.Items.Add("getting market info"));
+                        RpcClient.Market.BeginGetMarketInformation(MarketId.ToString(), ar3 =>
+                        {
+                            Market = RpcClient.Market.EndGetMarketInformation(ar3).MarketInformation;
+                            Dispatcher.BeginInvoke(() => button1.IsEnabled = true);
+                        }, null);
+                    }, null);
+                });
+            }, null);
+        }
+
+        private void OnMarketPricesStreamMessageReceived(object sender, MessageEventArgs<PriceDTO> e)
+        {
+            if (!_listening || _ordered || Market == null) return;
+            _ordered = true;
+
+            var order = new NewTradeOrderRequestDTO
+            {
+                MarketId = e.Data.MarketId,
+                BidPrice = e.Data.Bid,
+                OfferPrice = e.Data.Offer,
+                AuditId = e.Data.AuditId,
+                Quantity = Market.WebMinSize.GetValueOrDefault() + 1,
+                TradingAccountId = Account.TradingAccounts[0].TradingAccountId,
+                Direction = "buy"
+            };
+
+            Dispatcher.BeginInvoke(() => listBox1.Items.Add("price update arrived, making a new trade"));
+            RpcClient.TradesAndOrders.BeginTrade(order, ar =>
+            {
+                var result = RpcClient.TradesAndOrders.EndTrade(ar);
+                var newOrder = result.Orders.Length > 0 && result.Orders[0].OrderId == result.OrderId
+                    ? result.Orders[0]
+                    : null;
+
+                if (newOrder != null && newOrder.Status == 8 && newOrder.StatusReason == 140)
+                {
+                    Dispatcher.BeginInvoke(() =>
+                    {
+                        listBox1.Items.Add("the account is on a dealer watchlist!");
+                        listBox1.Items.Add("waiting for the order approval...");
+                    });
+                }
+            }, null);
+        }
+
+        private void OnOrdersStreamMessageReceived(object sender, MessageEventArgs<OrderDTO> e)
+        {
+            Dispatcher.BeginInvoke(() => listBox1.Items.Add("got the response from the approval console!"));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                button1.IsEnabled = false;
+                _listening = true;
+            });
+        }
+    }
+}
