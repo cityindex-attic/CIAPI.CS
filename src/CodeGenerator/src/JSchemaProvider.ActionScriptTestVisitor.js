d7bc805366c224ea3df1717eced51b43866f2c55
adrian.parker <adrian@iceknife.com>
2012-06-12 11:13:29 +0100

    add test directory command line arg; initial cut of test generation

--- src/CodeGenerator/src/JSchemaProvider.ActionScriptTestVisitor.js
+++ src/CodeGenerator/src/JSchemaProvider.ActionScriptTestVisitor.js
@@ -1,11 +1,11 @@
 ﻿/// <reference path="JSchemaProvider.js" />
 (function () {
     var outputString = "";
-    exports.ActionScriptVisitor = function () {
+    exports.ActionScriptTestVisitor = function () {
         this._lines = [];
         this.outputDir = "";
     };
-    exports.ActionScriptVisitor.prototype = {
+    exports.ActionScriptTestVisitor.prototype = {
         provider:{},
         normalizeKey:function (key) {
             if (key && (key.indexOf("#.") > -1 || key.indexOf("#/") > -1)) {
@@ -113,29 +113,71 @@
                     outputString = "";
                     this.writeLine("package uk.co.cityindex.dto");
                     this.writeLine("{");
+                    this.writeLine("import org.flexunit.asserts.assertEquals;");
+                    this.writeLine("import org.flexunit.asserts.assertNotNull;");
+                    this.writeLine("import org.flexunit.asserts.assertNull;");
+                    this.writeLine("import org.flexunit.asserts.assertTrue;");
                     this.writeLine("");
-                    var typeName = "class";
-                    this.writeLine("    [RemoteClass]");
                     if (current.value.description) {
                         this.writeLine("    /**");
-                        this.writeLine("     * " + current.value.description);
+                        this.writeLine("     * Test for " + current.key+".");
                         this.writeLine("     * ");
-                        this.writeLine("     * DO NOT EDIT -- code is generated from API metadata. Changes will be overwritten.");
+                        this.writeLine("     * DO NOT EDIT -- test is generated from API metadata. Changes will be overwritten.");
                         this.writeLine("     * ");
                         this.writeLine("     */");
                     }
-                    this.writeLine("    public " + typeName + " " + current.key + (current.value["extends"] ? (" extends " + this.normalizeKey(current.value["extends"])) : ""));
+                    this.writeLine("    public class Test" + current.key);
                     this.writeLine("    {");
+                    this.writeLine("        ");
+                    this.writeLine("        private var subject:"+current.key+";");
+                    this.writeLine("        ");
+                    this.writeLine("        [Before]");
+                    this.writeLine("        public function setUp():void");
+                    this.writeLine("        {");
+                    this.writeLine("            assertNull(\"Subject not null before test\", subject);");
+                    this.writeLine("            subject = new "+current.key+"();");
+                    this.writeLine("            assertNotNull(\"Subject null after instantiation\", subject);");
+                    this.writeLine("        }");
+                    this.writeLine("        ");
+                    this.writeLine("        [After]");
+                    this.writeLine("        public function tearDown():void");
+                    this.writeLine("        {");
+                    this.writeLine("            assertNotNull(\"Subject null after test\", subject);");
+                    this.writeLine("            subject = null;");
+                    this.writeLine("            assertNull(\"Subject not null after nullification\", subject);");
+                    this.writeLine("        }");
+                    this.writeLine("        ");
                     break;
 
                 case 5: // type property
                     var propertyType = this.resolveType(current.value);
+                    this.writeLine("        [Test]");
                     if (current.value.description) {
                         this.writeLine("        /**");
-                        this.writeLine("         * " + current.value.description);
+                        this.writeLine("         * Test we can get and set " + current.value.description);
                         this.writeLine("         */");
                     }
-                    this.writeLine("        public var " + nameLowerCaseLead(current.key) + ":" + propertyType + ";");
+                    this.writeLine("        public function canGetSet" + current.key + "():void");
+                    this.writeLine("        {");
+                    switch (propertyType){
+                        case "Number":
+                            this.writeLine("            assertTrue(\"Subject does not have NaN "+nameLowerCaseLead(current.key)+"\", isNaN(subject."+nameLowerCaseLead(current.key)+"));");
+                            this.writeLine("            var test"+current.key+":Number = 1;");
+                            this.writeLine("            subject."+nameLowerCaseLead(current.key)+" = test"+current.key+";");
+                            this.writeLine("            assertEquals(\"Subject does not have expected "+nameLowerCaseLead(current.key)+"\", 1, subject."+nameLowerCaseLead(current.key)+");");
+                            break;
+                        case "String":
+                            this.writeLine("            assertNull(\"Subject does not have null "+nameLowerCaseLead(current.key)+"\", subject."+nameLowerCaseLead(current.key)+");");
+                            this.writeLine("            var test"+current.key+":String = \"test\";");
+                            this.writeLine("            subject."+nameLowerCaseLead(current.key)+" = test"+current.key+";");
+                            this.writeLine("            assertEquals(\"Subject does not have expected "+nameLowerCaseLead(current.key)+"\", \"test\", subject."+nameLowerCaseLead(current.key)+");");
+                            break;
+                        default:
+                            this.writeLine("            //TODO generation not yet implemented for "+propertyType);
+                            break;
+                    }
+                    this.writeLine("        }");
+                    this.writeLine("        ");
                     break;
             }
         },
@@ -143,7 +185,7 @@
             switch (this.provider.stack.length) {
                 case 3:
                     var current = this.provider.peek();
-                    var arrayCount = 97; // ascii char code for 'a'
+//                    var arrayCount = 97; // ascii char code for 'a'
                     if (current.value["enum"]) {
                         // no constructor for an enum, but we do need constants to represent the enum values
                         if(current.value.hasOwnProperty("options")){
@@ -160,40 +202,40 @@
                     } else {
                         // parameterised constructor for pass through initialisation post JSON deserialisation
                         this.writeLine("");
+                        this.writeLine("        [Test]");
                         this.writeLine("        /**");
-                        this.writeLine("         * Parameterised constructor allows for instantiation via Trading API response.");
-                        this.writeLine("         * @param data deserialised JSON from TradingAPI response");
+                        this.writeLine("         * Test parameterised constructor instantiation.");
                         this.writeLine("         */");
-                        this.writeLine("        public function " + current.key + "(data:Object = null)");
+                        this.writeLine("        public function test" + current.key + "():void");
                         this.writeLine("        {");
-                        this.writeLine("            if(data)");
-                        this.writeLine("            {");
-
-                        var dto = current.value;
-                        if(dto.hasOwnProperty("properties")){
-                            var props = dto["properties"];
-                            for(var key in props){
-                                if(props.hasOwnProperty(key)){
-                                    var prop = props[key];
-                                    if(prop && prop.type && prop.type=='array'){
-                                        var iterVar = String.fromCharCode(arrayCount);
-                                        this.writeLine("                for(var "+iterVar+":int = 0; "+iterVar+" < data."+key+".length; "+iterVar+"++)");
-                                        this.writeLine("                {");
-                                        this.writeLine("                    "+nameLowerCaseLead(key)+".push(new "+this.normalizeKey(prop.items[0].$ref)+"(data."+key+"["+iterVar+"]));");
-                                        this.writeLine("                }");
-                                        arrayCount++;
-                                        if(arrayCount>122){
-                                            // we've used a...z, smells like error, or we have an API dev gone rogue
-                                            throw new Error("More than 26 child arrays!  "+current.key);
-                                        }
-                                    } else {
-                                        this.writeLine("                "+nameLowerCaseLead(key)+" = data."+key+";");
-                                    }
-                                }
-                            }
-                        }
-
-                        this.writeLine("            }");
+//                        this.writeLine("            if(data)");
+//                        this.writeLine("            {");
+//
+//                        var dto = current.value;
+//                        if(dto.hasOwnProperty("properties")){
+//                            var props = dto["properties"];
+//                            for(var key in props){
+//                                if(props.hasOwnProperty(key)){
+//                                    var prop = props[key];
+//                                    if(prop && prop.type && prop.type=='array'){
+//                                        var iterVar = String.fromCharCode(arrayCount);
+//                                        this.writeLine("                for(var "+iterVar+":int = 0; "+iterVar+" < data."+key+".length; "+iterVar+"++)");
+//                                        this.writeLine("                {");
+//                                        this.writeLine("                    "+nameLowerCaseLead(key)+".push(new "+this.normalizeKey(prop.items[0].$ref)+"(data."+key+"["+iterVar+"]));");
+//                                        this.writeLine("                }");
+//                                        arrayCount++;
+//                                        if(arrayCount>122){
+//                                            // we've used a...z, smells like error, or we have an API dev gone rogue
+//                                            throw new Error("More than 26 child arrays!  "+current.key);
+//                                        }
+//                                    } else {
+//                                        this.writeLine("                "+nameLowerCaseLead(key)+" = data."+key+";");
+//                                    }
+//                                }
+//                            }
+//                        }
+//
+//                        this.writeLine("            }");
                         this.writeLine("        }");
                         this.writeLine("");
                     }
@@ -203,7 +245,7 @@
                     this.writeLine("");
                     var myOutput = outputString;
                     var fs = require('fs');
-                    fs.open(this.outputDir + current.key + '.as', 'w', 666, function (e, id) {
+                    fs.open(this.outputDir + 'Test'+current.key + '.as', 'w', 666, function (e, id) {
                         fs.write(id, myOutput, 0, myOutput.length, 0, function (id) {
                             fs.close(id);
                         });
--- src/CodeGenerator/src/generate_actionscript.js
+++ src/CodeGenerator/src/generate_actionscript.js
@@ -1,25 +1,23 @@
 ﻿
 console.log("Initialising program....");
 
-// get the target output directory, make sure it has a trailing path separator
-var outDir = process.argv[2];
-var path = require('path');
-if(!outDir || outDir=='undefined'){
-    throw new Error("Please supply absolute output directory path as first command line argument.")
-}else{
-    var pathSeparator = (process.platform === 'win32' ? '\\' : '/');
-    if(outDir.substr(outDir.length-1)!=pathSeparator){
-        outDir += pathSeparator;
-    }
-    outDir = path.normalize(outDir);
+if(!process.argv || process.argv.length<4){
+    throw new Error("Please invoke with 2 command line arguments - absolute path to dto dir, absolute path to test dir");
 }
-console.log("Writing to "+outDir);
+
+// get the target src output directory, make sure it has a trailing path separator
+var outDir = cleansePath(process.argv[2]);
+console.log("Writing DTO to "+outDir);
+// get the target test output directory, make sure it has a trailing path separator
+var testOutDir = cleansePath(process.argv[3]);
+console.log("Writing Tests to "+testOutDir);
 
 // load required resources
 var schema = require("./meta/schema.js").schema;
 var schemaPatch = require("./meta/schema.patch.js").schemaPatch;
 var JSchemaProvider = require("./JSchemaProvider.js").JSchemaProvider;
 var ActionScriptVisitor = require("./JSchemaProvider.ActionScriptVisitor.js").ActionScriptVisitor;
+var ActionScriptTestVisitor = require("./JSchemaProvider.ActionScriptTestVisitor.js").ActionScriptTestVisitor;
 
 // patch in additional members to DTO
 console.log("patching DTO....");
@@ -55,7 +53,7 @@ if(schemaPatch.hasOwnProperty("properties")){
 }
 
 // parse the schema into ActionScript DTO
-console.log("visiting code....");
+console.log("visiting DTO....");
 
 var visitor = new ActionScriptVisitor();
 visitor.outputDir = outDir;
@@ -63,4 +61,31 @@ var provider = new JSchemaProvider(visitor);
 provider.schema = schema;
 provider.visit("root", schema, "schema");
 
+// parse the schema into ActionScript DTO Tests
+console.log("visiting Tests....");
+
+var testVisitor = new ActionScriptTestVisitor();
+testVisitor.outputDir = testOutDir;
+var testProvider = new JSchemaProvider(testVisitor);
+testProvider.schema = schema;
+testProvider.visit("root", schema, "schema");
+
 console.log("Fiat voluntas tua.");
+
+
+
+
+
+function cleansePath(dir) {
+    var path = require('path');
+    if (!dir || dir == 'undefined') {
+        throw new Error("Please supply absolute output directory path command line arguments.")
+    } else {
+        var pathSeparator = (process.platform === 'win32' ? '\\' : '/');
+        if (dir.substr(dir.length - 1) != pathSeparator) {
+            dir += pathSeparator;
+        }
+        dir = path.normalize(dir);
+    }
+    return dir;
+}
